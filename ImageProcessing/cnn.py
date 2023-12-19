d27dc45abd836dab6f74f0482e32b017c5c8251f
nimalolikong <free.glancer.duker@gmail.com>
2023-12-20 01:06:30 +0800

    修改readme

--- ImageProcessing/cnn.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('../')
-
-from ImageProcessing.hog import face_center as hog_face_center
-
-
-def face_center(filename, model):
-    return hog_face_center(filename, model)
