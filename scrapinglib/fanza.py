6345f4d389bb3cc227e812ce44836772bf95faad
nimalolikong <free.glancer.duker@gmail.com>
2023-12-19 20:11:08 +0800

    第一次提交

---
+++ scrapinglib/fanza.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+
+import re
+from lxml import etree
+from urllib.parse import urlencode
+from .parser import Parser
+from selenium.webdriver import ChromeOptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from urllib.parse import  quote
+class Fanza(Parser):
+    source = 'fanza'
+
+    expr_title = '//*[starts-with(@id, "title")]/text()'
+    expr_actor = "//td[contains(text(),'出演者')]/following-sibling::td/span/a/text()"
+    # expr_cover = './/head/meta[@property="og:image"]/@content'
+    expr_extrafanart = '//a[@name="sample-image"]/img/@src'
+    expr_outline = "//div[@class='mg-b20 lh4']/text()"
+    expr_outline2 = "//div[@class='mg-b20 lh4']//p/text()"
+    expr_outline_og = '//head/meta[@property="og:description"]/@content'
+    expr_runtime = "//td[contains(text(),'収録時間')]/following-sibling::td/text()"
+
+    def search(self, number):
+        self.number = number
+        self.outnumber = number
+        if self.specifiedUrl:
+            self.detailurl = self.specifiedUrl
+            durl = "https://www.dmm.co.jp/age_check/=/declared=yes/?"+ urlencode({"rurl": self.detailurl})
+            print(durl)
+            self.htmltree = self.getHtmlTree(durl)
+            result = self.dictformat(self.htmltree)
+            return result
+        # fanza allow letter + number + underscore, normalize the input here
+        # @note: I only find the usage of underscore as h_test123456789
+        #fanza_search_number = number
+        
+        #if fanza_search_number.startswith("h-"):
+        #    fanza_search_number = fanza_search_number.replace("h-", "h_")
+        #fanza_search_number = re.sub(r"[^0-9a-zA-Z_]", "", fanza_search_number).lower()
+        
+        page_url = self.getFanzaTrueUrlbySearchNumber(number)
+        self.detailurl = page_url
+        '''
+        修改为直接搜索number返回正确url
+
+        '''
+        
+        url = "https://www.dmm.co.jp/age_check/=/declared=yes/?"+ urlencode({"rurl": self.detailurl})
+        self.htmlcode = self.getHtml(url)
+        if self.htmlcode != 404 \
+                and 'Sorry! This content is not available in your region.' not in self.htmlcode:
+            if 'fn-popup' in self.htmlcode:
+                self.htmlcode = self.getTrueHtmlFromFanza(url)
+            self.htmltree = etree.HTML(self.htmlcode)
+            if self.htmltree is not None:
+               result = self.dictformat(self.htmltree)
+               return result
+        return 404
+    
+        
+    def getFanzaTrueUrlbySearchNumber(self,number):
+
+        '''
+        搜索前先进行httpurl-string格式转换
+        只影响日语字符
+        '''
+        search_number = quote(number)#使用quote函数进行转换
+        url = 'https://www.dmm.co.jp/search/=/searchstr='+search_number
+        print(url)
+        '''
+        这里f_number特指av番号形式，如果是动画不会影响搜索结果
+        '''
+        f_number = number
+        if search_number == number:
+          f_number = number.replace('-','').lower()
+          print(f'fanza小写番号{f_number}')
+        else:
+            print(f'动画标题名{f_number}')
+        search_htmlcode = self.getHtml(url)
+        
+        if search_htmlcode != 404 and f_number in search_htmlcode:
+            s_etree = etree.HTML(search_htmlcode)
+            true_url = s_etree.xpath('//*[@id="list"]/li[1]/div/p[2]/a/@href')
+            if len(true_url) != 0:
+                print('成功查找到商品链接')
+                return true_url[0]
+            else:
+                print('未能得到商品链接元素')
+        return 404
+    
+    def getTrueHtmlFromFanza(self,url):
+        '''
+        得到在fanza打广告期间正确html地址
+        '''
+        ans = url
+        options = ChromeOptions()
+        '''
+        默认使用http代理，如果开启其他代理请自行修改源码
+        '''
+        proxy = self.proxies['http'].replace('http://','')
+        print('selenuim 代理已经连通，地址：'+proxy)
+        options.add_argument(('--proxy-server=' + proxy))
+        '''
+        有时会反复进行ssl连接,请查看是否开启了全局连接
+        '''
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        time.sleep(1)
+        driver.find_element(By.XPATH,'/html/body/table/tbody/tr/td[2]/div[2]/div[1]/div/div[3]/p').click()
+        time.sleep(1)
+        ans = driver.page_source
+
+
+        driver.close()
+        return ans
+    def getAnimeFlag(self):
+        if "anime" in self.detailurl:
+            return True
+        return False
+    def getAnimeNum(self,htmltree):
+        return self.getFanzaString('品番：')
+    def getNum(self, htmltree):
+        # for some old page, the input number does not match the page
+        # for example, the url will be cid=test012
+        # but the hinban on the page is test00012
+        # so get the hinban first, and then pass it to following functions
+        
+        self.fanza_hinban = self.getFanzaString('品番：')
+        self.number = self.fanza_hinban
+        number_lo = self.number.lower()
+        if (re.sub('-|_', '', number_lo) == self.fanza_hinban or
+            number_lo.replace('-', '00') == self.fanza_hinban or
+            number_lo.replace('-', '') + 'so' == self.fanza_hinban
+        ):
+            self.number = self.number
+        
+        return self.outnumber
+
+    def getStudio(self, htmltree):
+        return self.getFanzaString('メーカー')
+
+    def getOutline(self, htmltree):
+        try:
+            result = self.getTreeElement(htmltree, self.expr_outline).replace("\n", "")
+            if result == '':
+                result = self.getTreeElement(htmltree, self.expr_outline2).replace("\n", "")
+            if "※ 配信方法によって収録内容が異なる場合があります。" == result:
+                result = self.getTreeElement(htmltree, self.expr_outline_og)
+            return result
+        except:
+            return ''
+
+    def getRuntime(self, htmltree):
+        return str(re.search(r'\d+', super().getRuntime(htmltree)).group()).strip(" ['']")
+
+    def getDirector(self, htmltree):
+        if "anime" not in self.detailurl:
+            return self.getFanzaString('監督：')
+        return ''
+    
+    def getActors(self, htmltree):
+        if "anime" in self.detailurl:
+            return self.getLabel(htmltree)
+        return super().getActors(htmltree)
+        
+
+    def getRelease(self, htmltree):
+        result = self.getFanzaString('発売日：')
+        if result == '' or result == '----':
+            result = self.getFanzaString('配信開始日：')
+        return result.replace("/", "-").strip('\\n')
+
+    def getTags(self, htmltree):
+        return self.getFanzaStrings('ジャンル：')
+
+    def getLabel(self, htmltree):
+        ret = self.getFanzaString('レーベル')
+        if ret == "----":
+            return ''
+        return ret
+
+    def getSeries(self, htmltree):
+        ret = self.getFanzaString('シリーズ：')
+        if ret == "----":
+            return ''
+        return ret
+    
+    def getCover(self, htmltree):
+        cover_number = self.number
+        try:
+            result = htmltree.xpath('//*[@id="' + cover_number + '"]/@href')[0]
+        except:
+            # sometimes fanza modify _ to \u0005f for image id
+            if "_" in cover_number:
+                cover_number = cover_number.replace("_", r"\u005f")
+            try:
+                result = htmltree.xpath('//*[@id="' + cover_number + '"]/@href')[0]
+            except:
+                # (TODO) handle more edge case
+                # print(html)
+                # raise exception here, same behavior as before
+                # people's major requirement is fetching the picture
+                raise ValueError("can not find image")
+        return result
+
+    def getExtrafanart(self, htmltree):
+        #if "anime" in self.detailurl:
+        #    data = htmltree.xpath('/html/body/table/tbody/tr/td[2]/div/table/tbody/tr/td[1]/div[6]')
+        #else:
+        #    data = htmltree.xpath('/html/body/table/tbody/tr/td[2]/div/table/tbody/tr/td[1]/div[1]/div[1]/div[2]')
+        data = htmltree.xpath('//*[@id="sample-image-block"]') 
+
+        if len(data) != 0:
+          url_list = data[0].xpath('//*[@name="sample-image"]/img/@src')
+          if len(url_list) != 0:
+                sheet = []
+                for img_url in url_list:
+                    url_cuts = img_url.rsplit('-', 1)
+                    sheet.append(url_cuts[0] + 'jp-' + url_cuts[1])
+                return sheet
+          
+        return ''
+
+    def getTrailer(self, htmltree):
+        htmltext = re.search(r'<script type=\"application/ld\+json\">[\s\S].*}\s*?</script>', self.htmlcode)
+        if htmltext:
+            htmltext = htmltext.group()
+            url = re.search(r'\"contentUrl\":\"(.*?)\"', htmltext)
+            if url:
+                url = url.group(1)
+                url = url.rsplit('_', 2)[0] + '_mhb_w.mp4'
+                return url
+        return ''
+
+    def getFanzaString(self, expr):
+        result1 = str(self.htmltree.xpath("//td[contains(text(),'"+expr+"')]/following-sibling::td/a/text()")).strip(" ['']")
+        result2 = str(self.htmltree.xpath("//td[contains(text(),'"+expr+"')]/following-sibling::td/text()")).strip(" ['']")
+        return result1+result2
+
+    def getFanzaStrings(self, string):
+        result1 = self.htmltree.xpath("//td[contains(text(),'" + string + "')]/following-sibling::td/a/text()")
+        if len(result1) > 0:
+            return result1
+        result2 = self.htmltree.xpath("//td[contains(text(),'" + string + "')]/following-sibling::td/text()")
+        return result2
